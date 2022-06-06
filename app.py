a9b4dbb249a54f9a5484a77776fb6b7d3931700c
kushal19057 <kushal19057@iiitd.ac.in>
2022-06-06 21:22:39 +0530

    removed google sheets API stuff.

--- app.py
+++ app.py
@@ -3,15 +3,14 @@ import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 import os
 
-scope = ["https://spreadsheets.google.com/feeds", 'https://www.googleapis.com/auth/spreadsheets',
-		"https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]
+# scope = ["https://spreadsheets.google.com/feeds", 'https://www.googleapis.com/auth/spreadsheets', "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]
 
 # refer - https://www.geeksforgeeks.org/using-google-sheets-as-database-in-python/
 
 # Assign credentials ann path of style sheet
-creds = ServiceAccountCredentials.from_json_keyfile_name("creds.json", scope)
-client = gspread.authorize(creds)
-sheet = client.open("alt-project-demo").sheet1
+# creds = ServiceAccountCredentials.from_json_keyfile_name("creds.json", scope)
+# client = gspread.authorize(creds)
+# sheet = client.open("alt-project-demo").sheet1
 
 
 app = Flask(__name__)
@@ -25,8 +24,9 @@ def index():
 
 @app.route('/api/v1/get', methods=['GET'])
 def get_data():
-    data = sheet.get_all_records()
-    print(data)
+    # data = sheet.get_all_records()
+    data = {}
+    # print(data)
     return make_response(jsonify({'response': data, 'message':  'retrieved data'}), 200)
 
 @app.route('/api/v1/get/<id>', methods=['GET'])
@@ -34,32 +34,34 @@ def get_data_by_id(id):
     print(id)
     # refer docs - https://docs.gspread.org/en/latest/user-guide.html#selecting-a-worksheet
     # get all records, then filter ...
-    data = sheet.get_all_records()
-    print(data)
+    # data = sheet.get_all_records()
+    # print(data)
     # refer list comprehension - https://www.programiz.com/python-programming/list-comprehension
-    refined = [row for row in data if row["id"]==int(id)]
+    # refined = [row for row in data if row["id"]==int(id)]
+    refined = {}
     return make_response(jsonify({'response': refined, 'message':  'retrieved data for {}'.format(id)}), 200)
 
 @app.route('/api/v1/post', methods=['POST'])
 def post_data():
-    data = request.get_json()
-    print(data)
-    row = [data["id"], data["name"], data["score"]]  # TODO add error handling
-    sheet.append_row(row)
+    # data = request.get_json()
+    data = {}
+    # print(data)
+    # row = [data["id"], data["name"], data["score"]]  # TODO add error handling
+    # sheet.append_row(row)
     return make_response(jsonify({'response': data, 'message': 'inserted data'}), 200)
 
 # dummy methods. TODO implement functionality
-@app.route('/api/v1/put/<id>', methods=['PUT'])
-def put_data_by_id(id):
-    print(id)
-    data = request.get_json()
-    print(data)
-    return make_response(jsonify({'response': data, 'message': 'updated data for {}'.format(id)}), 200)
-
-@app.route('/api/v1/delete/<id>', methods=['DELETE'])
-def delete_data_by_id(id):
-    print(id)
-    return make_response(jsonify({'response': {}, 'message': 'deleted data for {}'.format(id)}), 200)
+# @app.route('/api/v1/put/<id>', methods=['PUT'])
+# def put_data_by_id(id):
+#     print(id)
+#     data = request.get_json()
+#     print(data)
+#     return make_response(jsonify({'response': data, 'message': 'updated data for {}'.format(id)}), 200)
+#
+# @app.route('/api/v1/delete/<id>', methods=['DELETE'])
+# def delete_data_by_id(id):
+#     print(id)
+#     return make_response(jsonify({'response': {}, 'message': 'deleted data for {}'.format(id)}), 200)
 
 # references :
 # https://flask.palletsprojects.com/en/1.1.x/quickstart/
@@ -70,5 +72,6 @@ def delete_data_by_id(id):
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
-    port = int(os.getenv('PORT'))
+    # port = int(os.getenv('PORT'))
+    port = 5000
     app.run(threaded=True, port=port)
